2c5bbd3ab3383563d81574f5f8fbb25a1fc54164
MARIONGames <marionhake3@gmail.com>
2025-05-03 01:45:50 +0300

    Create DataStoreSync.js

---
+++ Server/Sky Battles/DataStoreSync.js
@@ -0,0 +1,31 @@
+const GameAPI = require("mgxl://SB/GameAPI.js")
+const Tween = GameAPI.TweenService
+const DataStoreService = GameAPI.DataStoreService
+const Binaries = require("https://mgxl.marion-games.com/Bin.js")
+
+const args1 = ( "TweenService", "DataStoreService.GetAsync", "DataStoreService.UpdateAsync" );
+const args2 = ( Binaries.DataModule, /0x000010000/ );
+const args3 = ( { } );
+
+DataStoreService.GetDataStore(args, args3.replace(data));
+DataStoreService.UpdateDataStore(args1.DataStore.Data, args2.DataModule, args);
+DataStoreService.RemoveDataStore(args3.replace(data), args2.DataModule, args1.DataStore.UpdateRequest, args,);
+
+/0xe3004/,
+/0xe3005/
+/0xe3006/
+/0xe3007/
+/0xe3008/
+/0xe3009/
+/0xe3010/
+/0xa3011/;
+
+CallBack(/0x010000/, args, this);
+console.confrim
+
+if (GameAPI.Failer)
+{
+    warn("Fucked Up!");
+    print("Good Byeeeeeeee!!!!!!!!!!!!!!!!");
+    
+}
